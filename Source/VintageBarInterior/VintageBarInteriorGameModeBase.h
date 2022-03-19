be190ba45097b5049724b6c79e407ae2891aad1c
Huseyn420 <huseyn20@gmail.com>
2022-03-19 17:08:42 +0300

    Fix rendering issues and optimize FPS

---
+++ Source/VintageBarInterior/VintageBarInteriorGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VintageBarInteriorGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VINTAGEBARINTERIOR_API AVintageBarInteriorGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
